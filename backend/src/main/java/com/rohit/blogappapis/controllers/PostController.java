1834effd0cef9766cfc9661c95cb58d832877573
rohitumadi <rohitumadi2000@gmail.com>
2023-10-28 12:48:27 +0530

    file download code

--- backend/src/main/java/com/rohit/blogappapis/controllers/PostController.java
+++ backend/src/main/java/com/rohit/blogappapis/controllers/PostController.java
@@ -6,14 +6,18 @@ import com.rohit.blogappapis.payloads.PostDto;
 import com.rohit.blogappapis.payloads.PostResponse;
 import com.rohit.blogappapis.services.FileService;
 import com.rohit.blogappapis.services.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -105,8 +109,14 @@ public class PostController {
         return new ResponseEntity<PostDto>(updatedPost,HttpStatus.OK);
 
     }
+@GetMapping(value = "/post/image/{imageName}",produces = MediaType.IMAGE_PNG_VALUE)
+    public void downloadImage(@PathVariable String imageName,
+                              HttpServletResponse response) throws IOException {
+    InputStream resource=this.fileService.getResource(path,imageName);
+    response.setContentType(MediaType.IMAGE_PNG_VALUE);
+    StreamUtils.copy(resource,response.getOutputStream());
 
-
+    }
 
 
 }
